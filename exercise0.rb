cfb3e5316cb38d9d8418e8eee23a461e4f1d7471
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-29 15:01:04 -0400

    complete exercise0

--- exercise0.rb
+++ exercise0.rb
@@ -1,1 +1,14 @@
+#Arrays
 fav_colors = ["red", "green", "yellow", "blue", "orange"]
+age = [22, 23, 56, 21, 78]
+coin_result = ["heads", "tails", "tails", "heads", "heads"]
+favourite_artists = ["Kanye West", "Childish Gambino", "Drake"]
+fav_colors1 = [red, green, yellow, blue, orange]
+
+#Hashs
+fav_movie = [title: => "Rush",:director => "Ron Howard", :year => "2013"]
+city1 = [:name => "Sydney", :country => "Australia",:population => "4.293 million"]
+city2 = [:name => "Paris", :country => "France",:population => "2.24 million"]
+city3 = [:name => "Toronto", :country => "Canada",:population => "2.61 million"]
+sibling1 = [:name => "Kelsey", :age => "22", :relation => "sister"]
+sibling2 = [:name => "Mackenzie", :age => "17", :relation => "sister"]
