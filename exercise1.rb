86a144a3ce84b754b08575b0d5805a150d3037e0
Adrian Pearman <adrianpearman12@gmail.com>
2017-03-29 15:23:55 -0400

    complete exercise1

---
+++ exercise1.rb
@@ -0,0 +1,14 @@
+coin_result = ["heads", "tails", "tails", "heads", "heads"]
+age = [22, 23, 56, 21, 78]
+fav_movie = {:title => "Rush", :director => "Ron Howard", :year => "2013"}
+
+print coin_result
+puts "\\"
+puts coin_result[0]
+puts "\\"
+print age.sort
+puts "\\"
+age << 0
+print age
+puts "\\"
+puts fav_movie[:year]
