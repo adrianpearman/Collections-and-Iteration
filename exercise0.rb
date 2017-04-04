f6cb0ed787cef02e24e31a5c17e4c2541e2f2f8c
Adrian Pearman <adrianpearman12@gmail.com>
2017-04-04 17:31:16 -0400

    update hash values

--- exercise0.rb
+++ exercise0.rb
@@ -7,8 +7,6 @@ fav_colors1 = [red, green, yellow, blue, orange]
 
 #Hashs
 fav_movie = [title: => "Rush",:director => "Ron Howard", :year => "2013"]
-city1 = [:name => "Sydney", :country => "Australia",:population => "4.293 million"]
-city2 = [:name => "Paris", :country => "France",:population => "2.24 million"]
-city3 = [:name => "Toronto", :country => "Canada",:population => "2.61 million"]
+cities = {:Sydney => 4293000, :Paris => 2240000, :Toronto => 2610000}
 sibling1 = [:name => "Kelsey", :age => "22", :relation => "sister"]
 sibling2 = [:name => "Mackenzie", :age => "17", :relation => "sister"]
